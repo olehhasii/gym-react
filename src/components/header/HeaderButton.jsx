c9b61809530543f2aa329b1b6df8d951ab49d4b9
Oleh Hasii <olehhasii@gmail.com>
2022-12-07 16:15:30 +0200

    Changed header/navigation/link to another style

--- src/components/header/HeaderButton.jsx
+++ src/components/header/HeaderButton.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const HeaderButton = ({ icon, onClickHandler }) => {
 	return (
 		<button
-			className='w-10 h-10 rounded-full flex items-center justify-center hover:border-2 hover:scale-110 duration-500 transition-all'
+			className='w-10 h-10 rounded-full flex items-center justify-center hover:border-2 hover:scale-110 duration-500 transition-all border-black'
 			onClick={onClickHandler}>
 			{icon}
 		</button>
--- src/components/navigation/NavigationLink.jsx
+++ src/components/navigation/NavigationLink.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const NavigationLink = ({ linkText, linkUrl, isFull, icon }) => {
-	const activeStyle = 'bg-green_bg text-green_txt';
+	const activeStyle = 'bg-green-400  text-black';
 	const fullLinkStyles =
-		'px-6 py-3 flex items-center rounded-lg transition-colors duration-300 hover:bg-green_bg hover:text-green_txt ';
+		'px-6 py-3 flex items-center rounded-lg transition-colors duration-300 hover:bg-green-400 border-2 border-black';
 	const smallLinkStyles =
-		'pt-3 pb-1 flex flex-col text-xs items-center rounded-lg transition-colors duration-300 hover:bg-green_bg hover:text-green_txt ';
+		'pt-3 pb-1 flex flex-col text-xs items-center rounded-lg transition-colors duration-300 hover:bg-green-400 border-2 border-black ';
 	const linkStyle = isFull ? fullLinkStyles : smallLinkStyles;
 
 	return (
